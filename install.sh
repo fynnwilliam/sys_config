78d231ff685fa633a0fae9c957324ed751982d20
fynnwilliam <fynnwilliam@gmail.com>
2022-03-28 06:50:52 -0400

    Create _enable_better_commit_message
    This helps with the readability of the script.

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ _install_homebrew() {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || exit 1
 }
 
-
 _configure_path() {
   [ $SHELL == "/bin/zsh" ] && shell_profile=~/.zprofile || shell_profile=~/.profile
   
@@ -12,16 +11,17 @@ _configure_path() {
   eval "$($1)"
 }
 
-
 # the next line implements a function for executing homebrew commands.
 _brew() { for line in $2; do brew $1 $line; done; }
 
+_enable_better_commit_message() {
+  echo "autocmd Filetype gitcommit setlocal spell textwidth=72" >> ~/.vimrc
+}
 
 [ $(command -v brew) ] || _install_homebrew
 
 [ $(arch)  == "arm64" ] && _configure_path'/opt/homebrew/bin/brew shellenv' ||
 [ $(uname) == "Linux" ] && _configure_path'/home/linuxbrew/.linuxbrew/bin/brew shellenv'
-
 
 raw_user_content="https://raw.githubusercontent.com/fynnwilliam/sys_config/main"
 
@@ -29,10 +29,7 @@ _brew "install" "$(curl -fsSL ${raw_user_content}/formulae)"
 _brew "tap" "$(curl -fsSL ${raw_user_content}/taps)"
 _brew "install --cask" "$(curl -fsSL ${raw_user_content}/casks)"
 
-
-# configure vim to help in writting better git commit messages.
-echo "autocmd Filetype gitcommit setlocal spell textwidth=72" >> ~/.vimrc
-
+_enable_better_commit_message
 
 # the next line installs oh-my-zsh.
 # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
